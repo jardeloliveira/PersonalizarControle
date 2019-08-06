b613f8c6542388d130fab31e2dd37b809e16bc08
Jardel Oliveira <jardell.oliveira@hotmail.com>
2019-08-05 21:47:52 -0300

    Sistema

---
+++ PersonalizarControl/PersonalizarControl.iOS/Controls/CustomBoxViewRenderer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using PersonalizarControl.iOS.Controls;
+using PersonalizarControl.Controls;
+using CoreGraphics;
+using System.ComponentModel;
+
+[assembly:ExportRenderer(typeof(CustomBoxView),typeof(CustomBoxViewRenderer))]
+namespace PersonalizarControl.iOS.Controls
+{
+    public class CustomBoxViewRenderer:BoxRenderer
+    {
+       
+        public override void Draw(CGRect rect)
+        {
+            //base.Draw(rect);
+            CustomBoxView control = (CustomBoxView)Element;
+            using(var context = UIGraphics.GetCurrentContext())
+            {
+                context.SetStrokeColor(new CGColor(0, 0, 0));
+                context.SetLineWidth((float)control.Espessura);
+
+                var recPath = new CGRect(0,0,200,200);
+
+                context.AddRect(recPath);
+                context.DrawPath(CGPathDrawingMode.Stroke);
+            }
+
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == CustomBoxView.EspessuraProperty.PropertyName)
+            {
+                SetNeedsDisplay();
+            }
+            //base.OnElementPropertyChanged(sender, e);
+            
+        }
+    }
+}
---
+++ PersonalizarControl/PersonalizarControl/Controls/CustomBoxView.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace PersonalizarControl.Controls
+{
+    public class CustomBoxView : BoxView
+    {
+        public static readonly BindableProperty EspessuraProperty = BindableProperty.Create<CustomBoxView,double>(
+                p=>p.Espessura, default(double)
+            );
+        public double Espessura
+        {
+            get { return (double)GetValue(EspessuraProperty); }
+            set { SetValue(EspessuraProperty, value); }
+        }
+
+    }
+}
